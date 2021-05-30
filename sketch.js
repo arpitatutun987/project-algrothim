10b42e94317c308975c0462298fc3fd2a63c1d5d
arpitatutun987 <83904693+arpitatutun987@users.noreply.github.com>
2021-05-30 16:04:51 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,14 +1,37 @@
-var box,box1;
+var box2,box1;
 
 function setup(){
-  createCanvas(400,400);
+  createCanvas(800,800);
 
-  box = createSprite(200,10,30,30);
+  box1 = createSprite(400,400,50,150);
+  box1.shapeColor = "red";
 
-  box1 = createSprite(10,200,50,50);
+  box2 = createSprite(100,100,150,50);
+  box2.shapeColor = "blue";
+
+  box1.debug = true;
+  box2.debug = true;
 }
 
 function draw(){
-  background("lightblue");
+  background(0);
+
+  box2.x = World.mouseX;
+  box2.y = World.mouseY;
+
+  if(box1.x - box2.x <= (box1.width + box2.width)/2
+  && box2.x - box1.x <= (box1.width + box2.width)/2
+  && box1.y - box2.y <= (box1.height + box2.height)/2
+  && box2.y - box1.y <= (box1.height + box2.height)/2 ){
+    
+    box1.shapeColor = "yellow";
+    box2.shapeColor = "yellow";
+  }
+
+  else {
+    box1.shapeColor = "red";
+    box2.shapeColor = "blue";
+  }
+
   drawSprites();
 }
